11c6d2c9cbfafc2c3bf0057342adcecd1a9902c9
Maximilian Stephan <stephan.maxi@icloud.com>
2023-03-30 20:11:42 +0100

    feat: :sparkles: Add Memory Info

--- src/index.mjs
+++ src/index.mjs
@@ -3,41 +3,92 @@ import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
 
-function App(props) {
-  return html`
-    <div>
-      ${props.cpus.map((cpu) => {
-        return html`<div class="bar">
-          <div class="bar-inner" style="width: ${cpu}%"></div>
-          <label>${cpu.toFixed(2)}%</label>
-        </div>`;
-      })}
-    </div>
-  `;
+function percentageToColor(percentage, maxHue = 0, minHue = 120) {
+    const hue = 120 - percentage;
+    return `hsl(${hue}, 100%, 50%)`;
 }
 
-let i = 0;
+const map = (x, in_min, in_max, out_min, out_max) => {
+return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
 
-// let update = async () => {
-//   let response = await fetch("/api/cpus");
-//   if (response.status !== 200) {
-//     throw new Error(`HTTP error! status: ${response.status}`);
-//   }
+const mapper = (range) => {
+    return (x) => {
+        return map(x, 0, 100, 0, range);
+    };
+};
 
-//   let json = await response.json();
-//   render(html`<${App} cpus=${json}></${App}>`, document.body);
-// };
+const m = mapper(120);
 
-// update();
-// setInterval(update, 200);
+function Proc({ cpu, total }) {
+    return html`<div
+      class="bar-inner"
+      style="
+        height: ${cpu}%;
+        opacity: ${cpu / 100};
+        background: ${percentageToColor(m(cpu))}
+      "
+    ></div>`;
+}
 
-let url = new URL("/realtime/cpus", window.location.href);
+function CpuStats({ cpus }) {
+    if (cpus) {
+        return html`
+          <div class="procs">
+            ${cpus
+              .map((cpu, i) => ({ cpu, i }))
+              .sort((a, b) => (a.cpu < b.cpu ? 1 : -1))
+              .map(({ cpu, i }) => {
+                return html`<div class="bar">
+                  <${Proc} cpu=${cpu} total=${cpus.length} />
+                  <label>${i}</label>
+                </div>`;
+              })}
+          </div>
+        `;
+      }
+}
+
+function MemStats({ mem_used, mem_total }) {
+    let mem_used_mb = (mem_used / 1_000_000).toFixed(2);
+    let mem_total_mb = (mem_total / 1_000_000).toFixed(2);
+    let mem_percent = (mem_used / mem_total * 100).toFixed(2);
+    let mem_free = ((mem_total_mb - mem_used_mb)).toFixed(0);
+    return html`
+        <div class="mem-bar">
+            <label>Total: ${mem_total_mb}MB</label>
+            <label>Used: ${mem_used_mb}MB</label>
+            <label>Free: ${mem_free}MB</label>
+            <div
+                class="mem-bar-inner"
+                style='
+                    width: ${mem_percent}%;
+                    opacity: ${mem_percent};
+                    background: ${percentageToColor(m(mem_percent))}
+                '
+            ></div>
+        </div>
+    `;
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    console.log("INFO: Using light mode, since system theme preferrs light.");
+    document.body.classList.add("light-mode");
+} else {
+    console.log("INFO: Using dark mode, since system theme preferrs dark.");
+}
+
+let url = new URL("/realtime/ressources", window.location.href);
 // http => ws
 // https => wss
 url.protocol = url.protocol.replace("http", "ws");
 
 let ws = new WebSocket(url.href);
 ws.onmessage = (ev) => {
-  let json = JSON.parse(ev.data);
-  render(html`<${App} cpus=${json}></${App}>`, document.body);
-};
+    let json = JSON.parse(ev.data);
+    let cpus = json.cpus.Vec32;
+    let mem_used = json.mem_used.U64;
+    let mem_total = json.mem_total.U64;
+    render(html`<${CpuStats} cpus=${cpus}></${CpuStats}>`, document.getElementById("cpu_stats"));
+    render(html`<${MemStats} mem_used=${mem_used} mem_total=${mem_total}></${MemStats}>`, document.getElementById("mem_stats"));
+}
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,23 @@ use axum::{
 };
 use sysinfo::{CpuExt, System, SystemExt};
 use tokio::sync::broadcast;
+use std::{collections::HashMap};
+use serde_with::serde_as;
+use serde::{Deserialize, Serialize};
 
-type Snapshot = Vec<f32>;
+#[serde_as]
+#[derive(Clone, Serialize, Deserialize, Debug)]
+enum HashValue {
+    U64(u64),
+    Vec32(Vec<f32>),
+}
+
+#[derive(Clone)]
+struct AppState {
+    tx: broadcast::Sender<Snapshot>,
+}
+
+type Snapshot = HashMap<String, HashValue>;
 
 #[tokio::main]
 async fn main() {
@@ -25,16 +40,21 @@ async fn main() {
         .route("/", get(root_get))
         .route("/index.mjs", get(indexmjs_get))
         .route("/index.css", get(indexcss_get))
-        .route("/realtime/cpus", get(realtime_cpus_get))
+        .route("/realtime/ressources", get(realtime_ressource_get))
         .with_state(app_state.clone());
 
     // Update CPU usage in the background
     tokio::task::spawn_blocking(move || {
         let mut sys = System::new();
         loop {
-            sys.refresh_cpu();
-            let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
-            let _ = tx.send(v);
+            sys.refresh_all();
+            let mut map: HashMap<String, HashValue> = HashMap::new();
+
+            let v: Vec<f32> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+            map.insert("cpus".to_string(), HashValue::Vec32(v));
+            map.insert("mem_used".to_string(), HashValue::U64(sys.used_memory()));
+            map.insert("mem_total".to_string(), HashValue::U64(sys.total_memory()));
+            let _ = tx.send(map);
             std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
         }
     });
@@ -44,11 +64,6 @@ async fn main() {
     println!("Listening on {addr}");
 
     server.await.unwrap();
-}
-
-#[derive(Clone)]
-struct AppState {
-    tx: broadcast::Sender<Snapshot>,
 }
 
 #[axum::debug_handler]
@@ -79,14 +94,14 @@ async fn indexcss_get() -> impl IntoResponse {
 }
 
 #[axum::debug_handler]
-async fn realtime_cpus_get(
+async fn realtime_ressource_get(
     ws: WebSocketUpgrade,
     State(state): State<AppState>,
 ) -> impl IntoResponse {
-    ws.on_upgrade(|ws: WebSocket| async { realtime_cpus_stream(state, ws).await })
+    ws.on_upgrade(|ws: WebSocket| async { realtime_ressource_stream(state, ws).await })
 }
 
-async fn realtime_cpus_stream(app_state: AppState, mut ws: WebSocket) {
+async fn realtime_ressource_stream(app_state: AppState, mut ws: WebSocket) {
     let mut rx = app_state.tx.subscribe();
 
     while let Ok(msg) = rx.recv().await {
